8ee2fdc2e0f8955fca42751b510cc023db2ba5e3
graciela23garcia <70188756+graciela23garcia@users.noreply.github.com>
2021-02-23 21:23:20 -0800

    created post route to post a note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,9 +4,23 @@ let jsonNotes = JSON.parse(notes);
 let db = require("../db/db.json");
 console.log(db);
 
+//this will get all of the notes for me 
 module.exports = (app) => {
-  // Get all notes
   app.get("/api/notes", (req, res) => {
     res.json(jsonNotes);
   });
+    // Post a new note
+    app.post("/api/notes", (req, res) => {
+      let newNote = {
+        id: Math.floor(Math.random() * 10000),
+        title: req.body.title,
+        text: req.body.text,
+      };
+      jsonNotes.push(newNote);
   
+      fs.writeFile("./db/db.json", JSON.stringify(jsonNotes), (err) => {
+        if (err) throw err;
+      });
+      res.send(newNote);
+    });
+   
